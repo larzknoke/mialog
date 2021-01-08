3c966d6eab3b6d359b235ca0e4c9bdf9aa2e8067
Lars Knoke <l.knoke@colorplus.de>
2021-01-08 14:02:07 +0100

    Sub-Navi

--- src/_assets/scripts/app.js
+++ src/_assets/scripts/app.js
@@ -1,10 +1,28 @@
-import { gsap } from 'gsap';
+import { gsap } from "gsap";
 
-gsap.from("#Teams", {duration: 1.5, rotation: -10,opacity: 0, x:20, onComplete:addTransition});
-gsap.from("#Organisation", {duration: 1.5, rotation: -12,opacity: 0, y:50, onComplete:addTransition});
-gsap.from("#Individuum", {duration: 2, rotation: 16,opacity: 0, x:-50, onComplete:addTransition});
-gsap.from("#Rand", {duration: 3, opacity: 0,scale: 0.95, x:5});
+gsap.from("#Teams", {
+	duration: 1.5,
+	rotation: -10,
+	opacity: 0,
+	x: 20,
+	onComplete: addTransition,
+});
+gsap.from("#Organisation", {
+	duration: 1.5,
+	rotation: -12,
+	opacity: 0,
+	y: 50,
+	onComplete: addTransition,
+});
+gsap.from("#Individuum", {
+	duration: 2,
+	rotation: 16,
+	opacity: 0,
+	x: -50,
+	onComplete: addTransition,
+});
+gsap.from("#Rand", { duration: 3, opacity: 0, scale: 0.95, x: 5 });
 
-function addTransition(){
-    this.targets()[0].classList.add("anim-completed");
+function addTransition() {
+	this.targets()[0].classList.add("anim-completed");
 }
